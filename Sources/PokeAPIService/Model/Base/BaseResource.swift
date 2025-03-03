569c367352055adf6adb358aa18b37a99f4a3bd5
Thomas George <private@thomas-george.com>
2025-03-03 17:10:43 +0100

    add base resource models

---
+++ Sources/PokeAPIService/Model/Base/BaseResource.swift
@@ -0,0 +1,13 @@
+//
+//  BaseResource.swift
+//  PokeAPIService
+//
+//  Created by Thomas George on 02/03/2025.
+//
+
+import Foundation
+
+struct BaseResource: Codable {
+    let name: String
+    let url: String
+}
---
+++ Sources/PokeAPIService/Model/Base/BaseResourceList.swift
@@ -0,0 +1,16 @@
+//
+//  ResourceList.swift
+//  PokeAPIService
+//
+//  Created by Thomas George on 01/03/2025.
+//
+
+import Foundation
+
+struct BaseResourceList: Codable {
+    let urls: [BaseResource]
+
+    enum CodingKeys: String, CodingKey {
+        case urls = "results"
+    }
+}
